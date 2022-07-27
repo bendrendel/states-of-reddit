df863bf75c97c77820860060afefaa65d4ddfabd
bendrendel <bfdrendel@gmail.com>
2022-07-27 11:32:43 -0600

    Remove boilerplate

--- __tests__/App.test.js
+++ __tests__/App.test.js
@@ -3,14 +3,3 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../src/app/store';
 import App from '../src/app/App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-
-  // eslint-disable-next-line testing-library/prefer-screen-queries
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
