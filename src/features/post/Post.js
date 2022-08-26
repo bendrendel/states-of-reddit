3146e2eb71012a0fb84f55cea09daf79ea350798
bendrendel <bfdrendel@gmail.com>
2022-08-26 15:25:20 -0600

    Add relevant data from mock data to subreddit and post pages

--- src/features/post/Post.js
+++ src/features/post/Post.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import './Post.css';
 import { fetchData } from './postSlice';
 
 function Post() {
     const dispatch = useDispatch();
+    const { subreddit } = useParams();
 
     const postSlice = useSelector(state => state.post);
 
@@ -14,15 +16,28 @@ function Post() {
 
     return (
         <div>
-            <p>Post</p>
-
             <p>isLoading: {postSlice.isLoading ? 'true' : 'false'}</p>
             <p>hasError: {postSlice.hasError ? 'true' : 'false'}</p>
-            
+            {postSlice.data && (
+                <div>
+                    <p>{`r/${subreddit} - posted by u/${postSlice.data[0].data.children[0].data.author} ${postSlice.data[0].data.children[0].data.created_utc} ago`}</p>
+                    <p>{postSlice.data[0].data.children[0].data.title}</p>
+                    
+                    {/\.jpg$/.test(postSlice.data[0].data.children[0].data.url) ? <img src={postSlice.data[0].data.children[0].data.url} /> : 'no image!'}
+                    
+                    <p>{postSlice.data[0].data.children[0].data.upvote_ratio * 100}% upvoted</p>
+                    <p>{postSlice.data[0].data.children[0].data.num_comments} comments</p>
 
-            <p>{postSlice.data && postSlice.data[0].data.children[0].data.title}</p>
-
-            {postSlice.data && postSlice.data[1].data.children.map(comment => <p>{comment.data.body}</p>)}
+                    {postSlice.data[1].data.children.map(comment => (
+                        <div>
+                            <p>{comment.data.author}</p>
+                            <p>{comment.data.created_utc} ago</p>
+                            <p>{comment.data.body}</p>
+                            <p>{comment.data.ups} upvotes</p>
+                        </div>
+                    ))}                    
+                </div>
+            )}
         </div>
     )
 }
--- src/features/subreddit/Subreddit.js
+++ src/features/subreddit/Subreddit.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Link, useParams, useRouteMatch } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import './Subreddit.css';
 import Search from '../search/Search';
@@ -25,7 +25,10 @@ function Subreddit() {
 
             {subredditSlice.data?.data.children.map(post => (
                 <div>
+                    <p>posted by {`u/${post.data.author}`} {post.data.created_utc} ago</p>
                     <p>{post.data.title}</p>
+                    <p>{post.data.ups} upvotes</p>
+                    <p>{post.data.num_comments} comments</p>
                     <Link to={post.data.permalink}>Post Link</Link>
                 </div>
             ))}
