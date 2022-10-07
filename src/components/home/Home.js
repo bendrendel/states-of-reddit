f618aa6bcb1d77def029228cf5cda20203cd410b
bendrendel <bfdrendel@gmail.com>
2022-10-07 14:24:33 -0600

    Update home page font and background gradient

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,8 +4,10 @@ import './Home.css';
 function Home() {
     return (
         <main className='home-page'>
-            <h1>Welcome to States of Reddit!</h1>
-            <p>The <em>fastest</em> way to browse the subreddits of the fifty U.S. states</p>
+            <header>
+                <h1>Welcome to States of Reddit!</h1>
+                <p>The <em>fastest</em> way to browse the subreddits of the fifty U.S. states</p>
+            </header>
         </main>
     )
 }
