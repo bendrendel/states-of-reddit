36b1ad52c01123cba40f949d258b7b95560a95e1
bendrendel <bfdrendel@gmail.com>
2022-10-16 13:21:56 -0600

    Simplify post selector for subreddit slice

--- src/features/subreddit/Subreddit.js
+++ src/features/subreddit/Subreddit.js
@@ -45,15 +45,15 @@ function Subreddit() {
                 <section className='subreddit-posts'>
                     <ul>
                         {posts.map(post => (
-                            <li key={post.data.id}>
-                                <Link to={post.data.permalink} className='post-preview'>
+                            <li key={post.id}>
+                                <Link to={post.permalink} className='post-preview'>
                                     <header>
-                                        <p>{`posted by u/${post.data.author} ${timeSince(post.data.created_utc)}`}</p>
+                                        <p>{`posted by u/${post.author} ${timeSince(post.created_utc)}`}</p>
                                     </header>
-                                    <h2>{unescape(post.data.title)}</h2>
+                                    <h2>{unescape(post.title)}</h2>
                                     <footer>
-                                        <p>{kmbt(post.data.ups)} {post.data.ups == 1 ? 'upvote' : 'upvotes'}</p>
-                                        <p>{kmbt(post.data.num_comments)} {post.data.num_comments == 1 ? 'comment' : 'comments'}</p>                            
+                                        <p>{kmbt(post.ups)} {post.ups == 1 ? 'upvote' : 'upvotes'}</p>
+                                        <p>{kmbt(post.num_comments)} {post.num_comments == 1 ? 'comment' : 'comments'}</p>                            
                                     </footer>
                                 </Link>                            
                             </li>
