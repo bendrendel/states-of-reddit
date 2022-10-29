e5ee5b87f299183159d778ff2bde006350ab91bc
bendrendel <bfdrendel@gmail.com>
2022-10-28 22:09:38 -0600

    Refactor post page to add no comments indicator component

---
+++ src/features/post/no-comments-indicator/NoCommentsIndicator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './NoCommentsIndicator.css';
+
+function NoCommentsIndicator() {
+    return (
+        <div className='no-comments-indicator'>No comments yet</div>
+    )
+};
+
+export default NoCommentsIndicator;
