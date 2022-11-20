a838477b1ccb60da654f92af95a7440a25427942
bendrendel <bfdrendel@gmail.com>
2022-11-20 11:41:59 -0700

    Put hover effects in media queries to remove from mobile

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import App from './app/App';
 import { store } from './app/store';
 import reportWebVitals from './reportWebVitals';
 
-import './index.css';
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 
