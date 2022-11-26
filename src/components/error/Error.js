915755a77d5bfb0bd688e40a105e1a0a2cc478ec
bendrendel <bfdrendel@gmail.com>
2022-11-25 17:25:46 -0700

    Add go back option to error component

--- src/components/error/Error.js
+++ src/components/error/Error.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import './Error.css';
 
 function Error() {
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.goBack();
+    }
+
     return (
         <div className='error'>
             <p>Sorry, we're having trouble loading the requested data.</p>
-            <p>Please try again later.</p>
+            <button onClick={handleClick}>Go Back</button>
         </div>
     );
     
